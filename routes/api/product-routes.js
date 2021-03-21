70b311d4e1b88c723b7c078aaa6cbfdc79b4eb4f
AverieBeltran <averiebeltran@github.com>
2021-03-20 19:48:52 -0500

    created product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,59 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
     // find all products
     // be sure to include its associated Category and Tag data
+    Product.findAll({
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        include: [
+            {
+                model: Category,
+                attributes: ['id', 'category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'tag_name']
+            }
+        ]
+    })
+    .then(productData => res.json(productData))
+    .catch(error => {
+        console.log(error);
+        res.status(500).json(error);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
     // find a single product by its `id`
     // be sure to include its associated Category and Tag data
+    Product.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        include: [
+            {
+                model: Category,
+                attributes: ['id', 'category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'tag_name']
+            }
+        ]
+    })
+    .then(productData => {
+        if (!productData){
+            res.status(404).json({
+                message: "No product found with this ID"
+            });
+            return;
+        }
+        res.json(productData);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json(error);
+    });
 });
 
 // create new product
